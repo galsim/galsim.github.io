fb7524f1fa03a0da74b01748b1fa77ac583b934e
galsim <golubev.mak.ser@gmail.com>
2019-06-18 14:34:22 +0300

    Update

--- six/wp-content/themes/ma-mobile/section-menuside.php
+++
@@ -1,8 +0,0 @@
-<div class="menuside">
-    <div class="logo">
-        <div></div>
-    </div>
-    <div class="menu">
-        <?php wp_nav_menu(); ?>
-    </div>
-</div>
--- six/wp-content/themes/ma/section-centerside.php
+++
@@ -1,9 +0,0 @@
-<div class="centerside table">
-    <div class="inner">
-        <h2 class="tt"><?php the_title(); ?></h2>
-        <div class="pic">
-            <img src="<?php the_post_thumbnail_url( $size ); ?>" width="500">
-        </div>
-        <div class="info"><?php the_field( 'page__subtitle' ); ?></div>
-    </div>
-</div>
