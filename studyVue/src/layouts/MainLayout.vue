c5206d1bd49451f32da197e99966b8c7aa3e4d2f
maksim <golubev.mak.ser@gmail.com>
2019-10-05 12:41:04 +0300

    dlksfmlkskf

---
+++ studyVue/src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-main-layout">
+
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+    <navbar @click="isOpen = !isOpen" />
+    <sidebar v-model=" isOpen" />
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+import navbar from '@/components/app/navbar'
+import sidebar from '@/components/app/sidebar'
+export default {
+  name: 'main-layout',
+  data() {
+    return {
+      isOpen: true
+    }
+  },
+  components: {
+    navbar,
+    sidebar
+  }
+}
+</script>
+
---
+++ studyVue/src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dateFilter from '@/filters/date.filter'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+import messagePlugin from '@/utils/message.plugin'
+
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCkLZSMTRdOpeRLYb4qCPAfd3fjH1-LakY",
+  authDomain: "vue-crm-study-56943.firebaseapp.com",
+  databaseURL: "https://vue-crm-study-56943.firebaseio.com",
+  projectId: "vue-crm-study-56943",
+  storageBucket: "",
+  messagingSenderId: "825251025447",
+  appId: "1:825251025447:web:3f74cb130d58cdf1"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
